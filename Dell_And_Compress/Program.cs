41e193f1827dedb5fb901c602ba75e05e0433a4d
Davidson Goncalves <davidson.cg7@gmail.com>
2021-07-01 18:40:52 -0300

    Project Start

---
+++ Dell_And_Compress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dell_And_Compress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
