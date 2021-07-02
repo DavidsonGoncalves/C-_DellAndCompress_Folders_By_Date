ceab6c19fd70b1a679f8fdd85fd6bf06b538867f
Davidson Goncalves <davidson.cg7@gmail.com>
2021-07-01 22:25:06 -0300

    Create a log Method and write comand

--- Dell_And_Compress/Program.cs
+++ Dell_And_Compress/Program.cs
@@ -11,39 +11,49 @@ namespace Dell_And_Compress
     {
         static void Main()
         {
-
-            var OriginalPath = new DirectoryInfo(@"C:\temp\Folder");
+            var OriginalPath = new DirectoryInfo(@"D:\!Documents\Projects\Project DeleteFolders\Folder");
 
             Zip_File(OriginalPath.ToString());
 
             Delete_OldFile(OriginalPath);
-            
-            Console.ReadKey();
-
 
         }
 
 
-        public static void Zip_File(string path)
+        public static void Zip_File(string path) 
         {
             IEnumerable<string> folder = Directory.GetDirectories(path);
 
-            string lastFolder = folder.Last().ToString();
+            string lastFolder = folder.Last().ToString(); 
 
-            ZipFile.CreateFromDirectory(lastFolder, lastFolder + ".zip", CompressionLevel.Optimal, true);
-            Directory.Delete(lastFolder, true);
+            ZipFile.CreateFromDirectory(lastFolder, lastFolder + ".zip", CompressionLevel.Optimal, true);  
+            Directory.Delete(lastFolder, true); 
 
+            string LogText = $"[{DateTime.Now.ToString()}] - File Compressed Succssesfuly!";  
+            Log_Write(LogText); 
         }
 
         public static void Delete_OldFile(DirectoryInfo path)
         {
             var files = path.GetFiles();
-            var Files_Order = files.OrderBy(x => x.CreationTime);
-            var frist_File = Files_Order.First();
+            var Files_Order = files.OrderBy(x => x.CreationTime); 
+            var frist_File = Files_Order.First(); 
 
-            File.Delete(frist_File.ToString());
+            File.Delete(frist_File.ToString()); 
 
+            DateTime now = DateTime.Now;
+            string LogText = $"[{DateTime.Now.ToString()}] - Older File Deleted succssesfuly!"; 
+            Log_Write(LogText); 
         }
 
+        public static void Log_Write(string text)
+        {
+            string pathLog = @"D:\!Documents\Projects\Project DeleteFolders\Event_Log.txt"; 
+
+            using (StreamWriter sw = File.AppendText(pathLog))  
+            {
+                sw.WriteLine(text); 
+            }
+        }
     }
 }
